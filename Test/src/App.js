bc06c2a80c342b3aed0f06eb08d4ce0c9791030b
Gimhub365 <kimeunsik365@gmail.com>
2023-02-01 14:12:11 +0900

    test upload

---
+++ Test/src/App.js
@@ -0,0 +1,17 @@
+import Nav from './components/Nav'
+import Gallery from './components/Gallery'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Nav />
+        <Gallery />
+      </div>
+
+    </>
+  );
+}
+
+export default App;
